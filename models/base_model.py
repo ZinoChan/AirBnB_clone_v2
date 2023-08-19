f71934baefa7dada3275f820a5e68f9fb941cbad
ZinoChan <devzinochan@gmail.com>
2023-08-19 18:41:10 +0100

    refactor __init__ in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,29 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+        from models import storage
+
+        if len(kwargs) != 0:
+            self._assign_attributes(kwargs)
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls = (str(type(self)).split(".")[-1]).split("'")[0]
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
+
         self.updated_at = datetime.now()
         storage.save()
 
@@ -37,8 +36,16 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
+
+    def _assign_attributes(self, attrubutes):
+        """Assigns the attrubutes from kwargs"""
+        formatStr = "%Y-%m-%dT%H:%M:%S.%f"
+        for key, value in attrubutes.items():
+            if key == "created_at" or key == "updated_at":
+                self.__dict__[key] = datetime.strptime(value, formatStr)
+            else:
+                self.__dict__[key] = value
